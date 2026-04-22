c4e26ddf986e194317ac5f7073cdef1c85fc0334
Neethu C <sinetsoftware2025@gmail.com>
2026-04-22 20:29:15 +0530

    fix api import path in tenders page

--- server.js
+++ server.js
@@ -1,45 +1,45 @@
-require("dotenv").config();
-const express = require("express");
-const authRoutes = require("./routes/authRoutes");
-const userRoutes = require("./routes/userRoutes");
-const roleRoutes = require("./routes/roleRoutes");
-const permissionRoutes = require("./routes/permissionRoutes");
-const adminRoutes = require("./routes/adminRoutes");
-const orgRoutes = require("./routes/organization");
-const subscriptionRoutes = require("./routes/subscriptionRoutes");
-const tenderRoutes = require("./routes/tenderRoutes");
+  require("dotenv").config();
+  const express = require("express");
+  const authRoutes = require("./routes/authRoutes");
+  const userRoutes = require("./routes/userRoutes");
+  const roleRoutes = require("./routes/roleRoutes");
+  const permissionRoutes = require("./routes/permissionRoutes");
+  const adminRoutes = require("./routes/adminRoutes");
+  const orgRoutes = require("./routes/organization");
+  const subscriptionRoutes = require("./routes/subscriptionRoutes");
+  const tenderRoutes = require("./routes/tenderRoutes");
 
 
-const cors = require("cors");
-require("dotenv").config();
+  const cors = require("cors");
+  require("dotenv").config();
 
-const app = express();
+  const app = express();
 
-app.use(cors());
-app.use(express.json());
+  app.use(cors());
+  app.use(express.json());
 
-app.use("/api/auth", authRoutes);
-app.use("/api", userRoutes);
+  app.use("/api/auth", authRoutes);
+  app.use("/api", userRoutes);
 
 
-app.use("/api", roleRoutes);
-app.use("/api", permissionRoutes);
+  app.use("/api", roleRoutes);
+  app.use("/api", permissionRoutes);
 
-app.use("/api/organizations", orgRoutes);
-app.use("/api/subscriptions", subscriptionRoutes);
-app.use("/api/tenders", tenderRoutes);
-app.use("/api/admin", adminRoutes);
+  app.use("/api/organizations", orgRoutes);
+  app.use("/api/subscriptions", subscriptionRoutes);
+  app.use("/api/tenders", tenderRoutes);
+  app.use("/api/admin", adminRoutes);
 
-app.get("/", (req, res) => {
-  res.send("Procubid Admin Backend Running");
-});
+  app.get("/", (req, res) => {
+    res.send("Procubid Admin Backend Running");
+  });
 
-const PORT = process.env.PORT || 5000;
+  const PORT = process.env.PORT || 5000;
 
-// app.listen(PORT, () => {
-//   console.log(`Server running on http://localhost:${PORT}`);
-// });
+  // app.listen(PORT, () => {
+  //   console.log(`Server running on http://localhost:${PORT}`);
+  // });
 
-app.listen(PORT, "0.0.0.0", () => {
-  console.log(`Server running on ${PORT}`);
-});
+  app.listen(PORT, "0.0.0.0", () => {
+    console.log(`Server running on ${PORT}`);
+  });
